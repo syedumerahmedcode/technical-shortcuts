53fef7c018d8364cf909f35af3131bb69d769bb7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-22 11:35:09 +0200

    feature: linux/git/gcf.sh

--- linux/git/gcf.sh
+++ linux/git/gcf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 read -p "Enter the commit message for feature: "  commitmessage
 echo "The commit message for feature part is -----------" $commitmessage
 git commit -m"feature: $commitmessage"  $1
