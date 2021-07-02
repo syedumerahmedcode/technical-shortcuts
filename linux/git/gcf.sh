9f5cf7567fdaa6304f8a736ab9cbac2229bdb512
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-07-03 00:16:38 +0200

    Feature: Adapted the commit messages for git commit bash scripts.

--- linux/git/gcf.sh
+++ linux/git/gcf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Enter the commit message for feature: "  commitmessage
 echo "The commit message for feature part is -----------" $commitmessage
-git commit -m"feature: $commitmessage"  $1
+git commit -m"Feature: $commitmessage"  $1
--- linux/git/gct.sh
+++ linux/git/gct.sh
@@ -2,4 +2,4 @@
 
 read -p "Enter the commit message for test: "  commitmessage
 echo "The commit message for test part is -----------" $commitmessage
-git commit -m"test: $commitmessage"  $1
+git commit -m"Test: $commitmessage"  $1
